f81b8d377863dc9164df04df78f04779e4ddb39e
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-07-04 16:13:27 +0100

    Added responsiveness to improve viewing on mobile

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props) {
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4" key={index}>
+              <div className="col-6 col-md-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img src={photo.src.landscape} className="image-fluid" alt={props.keyword} />
                 </a>
