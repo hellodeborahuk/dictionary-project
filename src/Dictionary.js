595f7cb03a61109bca5d0f92651ff057418e306d
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-24 10:56:29 +0000

    hidden api key

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,6 +5,7 @@ import Photos from "./Photos";
 import "./Dictionary.css";
 import { FaSearch } from "react-icons/fa";
 
+
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
@@ -19,18 +20,18 @@ export default function Dictionary(props) {
     setPhotos(response.data.photos);
   }
 
+
+
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
 
-    const pexelsApiKey =
-      "563492ad6f917000010000019d1cd61598fb4587bf22c0e09637489c";
+    const pexelsApiKey = `${process.env.REACT_APP_IMAGE_API_KEY}`;
       let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
       let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
 
           axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
-
   }
 
   function handleSubmit(event) {
